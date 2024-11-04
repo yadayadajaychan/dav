d64914fc0a89fb56066f8c7b6472be11565ecedf
Ethan Cheng <ethan@nijika.org>
2024-11-03 20:04:16 -0800

    add sevenSegDigit

--- sevenSegDigit.sv
+++ sevenSegDigit.sv
@@ -1,17 +1,32 @@
 module sevenSegDigit(
-	// TODO: define your input and output ports
-
+	input [3:0] digit,
+	output reg [7:0] displayBits
 );
 
-	// The following block contains the logic of your combinational circuit
-	always_comb begin
-		// TODO: fill out the case construct below to output the correct seven-segment display bits
-		case (/* */)
-			
-			default: begin // this is the "catch-all" case - if none of the above cases match, this will execute
-				/* TODO: set your output bits */
-
-			end
+	always @(*) begin
+		case (digit)
+		4'd0:
+			displayBits = 8'b11000000;
+		4'd1:
+			displayBits = 8'b11111001;
+		4'd2:
+			displayBits = 8'b10100100;
+		4'd3:
+			displayBits = 8'b10110000;
+		4'd4:
+			displayBits = 8'b10011001;
+		4'd5:
+			displayBits = 8'b10010010;
+		4'd6:
+			displayBits = 8'b10000010;
+		4'd7:
+			displayBits = 8'b11011000;
+		4'd8:
+			displayBits = 8'b10000000;
+		4'd9:
+			displayBits = 8'b10010000;
+		default:
+			displayBits = 8'bXXXXXXXX;
 		endcase
 	end
 
