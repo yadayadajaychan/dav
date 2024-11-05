95ef8230afc9ae52a533ac655575e4d2aa025960
Ethan Cheng <ethan@nijika.org>
2024-11-04 21:16:38 -0800

    make displayEncoder output little endian

--- displayEncoder.sv
+++ displayEncoder.sv
@@ -5,13 +5,12 @@ module displayEncoder(
 	reg [19:0] tmp;
 	reg [3:0] bcd [0:5];
 
-	// most significant digit is displayBits[0]
-	sevenSegDigit digit0(bcd[0], displayBits[5]);
-	sevenSegDigit digit1(bcd[1], displayBits[4]);
-	sevenSegDigit digit2(bcd[2], displayBits[3]);
-	sevenSegDigit digit3(bcd[3], displayBits[2]);
-	sevenSegDigit digit4(bcd[4], displayBits[1]);
-	sevenSegDigit digit5(bcd[5], displayBits[0]);
+	sevenSegDigit digit0(bcd[0], displayBits[0]);
+	sevenSegDigit digit1(bcd[1], displayBits[1]);
+	sevenSegDigit digit2(bcd[2], displayBits[2]);
+	sevenSegDigit digit3(bcd[3], displayBits[3]);
+	sevenSegDigit digit4(bcd[4], displayBits[4]);
+	sevenSegDigit digit5(bcd[5], displayBits[5]);
 
 	always @(*) begin
 		tmp = result;
