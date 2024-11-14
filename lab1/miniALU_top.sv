05b2c37900d3bb2eaf9d48f5d825dfc5d54de3c1
ahjcgit <ahjcuniv@gmail.com>
2024-11-13 19:04:06 -0800

    YOOOOO COMMIT`

--- lab1/miniALU_top.sv
+++ lab1/miniALU_top.sv
@@ -1,7 +1,7 @@
 module miniALU_top (
 	// TODO: define your input and output ports
 	input [9:0] in,
-	output [7:0] displayBits [0:5]
+	output logic [7:0] displayBits [0:5]
 );
 	wire [19:0] result;
 	miniALU ALU(in[9:6], in[5:2], in[1], in[0], result);
--- lab1/miniALU_top_tb.sv
+++ lab1/miniALU_top_tb.sv
@@ -1,10 +1,12 @@
 `timescale 1ns / 1ns
-module miniALU_top_tb;
+module miniALU_top_tb(output [7:0] displayBits [0:5]);
 	reg [9:0] switches;
-	wire [7:0] displayBits [0:5];
+	//wire [7:0] displayBits [0:5];
 
 	miniALU_top DUT(switches, displayBits);
-
+	
+		
+		
 	initial begin
 		switches = 10'b1100001100; #10; // 12 + 3
 		switches = 10'b1100001101; #10; // 12 - 3
@@ -12,11 +14,11 @@ module miniALU_top_tb;
 		switches = 10'b1100001111; #10; // 12 >> 3
 	end
 
-	initial begin
-		$dumpfile("dump.vcd");
-		for (integer i=0; i<6; i=i+1) begin
-			$dumpvars(0, displayBits[i]);
-		end
-		$dumpvars(0, switches);
-	end
+//	initial begin
+//		$dumpfile("dump.vcd");
+//		for (integer i=0; i<6; i=i+1) begin
+//			$dumpvars(0, displayBits[i]);
+//		end
+//		$dumpvars(0, switches);
+//	end
 endmodule
