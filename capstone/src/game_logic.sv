3b1448af5fcb02dd96be61394a0c5b785e8008d1
ahjcgit <ahjcuniv@gmail.com>
2025-05-15 11:47:00 -0700

    Actually added game_logic.sv

---
+++ capstone/src/game_logic.sv
@@ -0,0 +1,36 @@
+module game_logic(
+    input [3:0] direction,
+    input rst,
+    input clk
+
+    output logic [3:0] grid [0:15],
+    output logic [1:0] state
+);
+
+    reg [3:0] matrix [0:15];       // 4x4 matrix stored as a 1D array
+    reg [3:0] transposed [0:15];   // Output transposed matrix
+
+    always @ (posedge clk or negedge rst) begin
+        if (!rst)
+            state <= 0;
+        else
+            case (direction)
+                4'b0000: begin // No input
+
+                end
+                4'b0001: begin // Right input
+                    
+                end
+                4'b1000: begin // Left input
+                
+                end
+                4'b0100: begin // Up input
+                
+                end
+                4'b0010: begin // Down input
+                
+                end
+            endcase
+    end
+
+endmodule
